786b6a5011c057c9334046ad0205a651ba216633
unknown <karthikmech2496@gmail.com>
2020-12-04 00:17:18 +0530

    scoialMedia accounts

---
+++ assets/resources/data.js
@@ -0,0 +1,29 @@
+var data = {
+	socialMedia : {
+		mail : {
+			href : "agamfilmco@gmail.com",
+			css : "fa-envelope-square",
+			text : "Envelope"
+		},
+		whatsapp : {
+			href : "https://wa.me/message/5UISCQSNBXTTL1",
+			css : "fa-envelope-square",
+			text : "Whatsapp"
+		},
+		twitter : {
+			href : "https://twitter.com/agamfilm?s=08",
+			css : "fa-twitter",
+			text : "Twitter"
+		},
+		telegram : {
+			href : "https://t.me/agamfilmco",
+			css : "fa-telegram",
+			text : "Telegram"
+		},
+		instagram : {
+			href : "https://instagram.com/agamfilm.co?igshid=11gaalq7h0vg",
+			css : "fa-instagram",
+			text : "Instagram"
+		}
+	}
+}
