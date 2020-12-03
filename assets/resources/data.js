6e3e75fad01f18804bf5f7bef4982a1a122048ee
unknown <karthikmech2496@gmail.com>
2020-12-04 02:45:40 +0530

    background img

--- assets/resources/data.js
+++ assets/resources/data.js
@@ -1,28 +1,28 @@
 var data = {
 	socialMedia : {
 		mail : {
-			href : "agamfilmco@gmail.com",
-			css : "fa-envelope-square",
+			href : "mailto:agamfilmco@gmail.com",
+			css : "far fa-envelope",
 			text : "Envelope"
 		},
 		whatsapp : {
 			href : "https://wa.me/message/5UISCQSNBXTTL1",
-			css : "fa-envelope-square",
+			css : "fab fa-whatsapp",
 			text : "Whatsapp"
 		},
 		twitter : {
 			href : "https://twitter.com/agamfilm?s=08",
-			css : "fa-twitter",
+			css : "fab fa-twitter",
 			text : "Twitter"
 		},
 		telegram : {
 			href : "https://t.me/agamfilmco",
-			css : "fa-telegram",
+			css : "fab fa-telegram-plane",
 			text : "Telegram"
 		},
 		instagram : {
 			href : "https://instagram.com/agamfilm.co?igshid=11gaalq7h0vg",
-			css : "fa-instagram",
+			css : "fab fa-instagram",
 			text : "Instagram"
 		}
 	}
